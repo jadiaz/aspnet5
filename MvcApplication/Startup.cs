430b2d994b3cb64134081939da047f277eb0f990
Jose Diaz <jose.a.diaz@aguaseca.com>
2015-02-25 16:52:50 -0700

    add diagnostic support

--- MvcApplication/Startup.cs
+++ MvcApplication/Startup.cs
@@ -8,6 +8,16 @@ namespace KWebStartup
     {
         public void Configure(IApplicationBuilder app)
         {
+            // Use default error page
+            app.UseErrorPage();
+
+            // Show runtime information on a page (DEVELOPMENT)
+            app.UseRuntimeInfoPage();
+
+            // Use custom error handler to catch exceptions and log them
+            app.UseErrorHandler("/error.html");
+
+            // Add services to pipeline
             app.UseServices(services =>
             {
                 services.AddMvc();
